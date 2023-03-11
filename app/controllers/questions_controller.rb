6d995efeb7426c9173d187b5b7a8051b56d03ad6
Marine Coltel <marine_coltel@hotmail.fr>
2023-03-11 05:56:43 +0100

    Stupid Coaching exercise without optional

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @answer = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work!"]
+    if params[:question].include? '?'
+      @answer = @answer[1]
+    elsif params[:question] == 'I am going to work'
+      @answer = @answer[0]
+    else
+      @answer = @answer[2]
+    end
+  end
+
+end
